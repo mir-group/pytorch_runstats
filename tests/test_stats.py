19320ad5030508f1828c4a00708b292ec27d2168
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-04-01 10:15:24 -0600

    Initial failing weighted tests

--- tests/test_stats.py
+++ tests/test_stats.py
@@ -48,6 +48,9 @@ class StatsTruth:
         if accumulate_by is None:
             accumulate_by = torch.zeros(len(batch), dtype=torch.long)
 
+        if self.weighted:
+            assert weight is not None
+
         if hasattr(self, "_state"):
             self._state = torch.cat((self._state, batch), dim=0)
             self._acc = torch.cat((self._acc, accumulate_by), dim=0)
@@ -64,6 +67,7 @@ class StatsTruth:
 
         if self.weighted:
             mul_by = scatter(weight, accumulate_by, dim=0)
+            mul_by.sum(dim=self._reduce_dims)
         else:
             mul_by = torch.bincount(accumulate_by)
             for d in self._reduce_dims:
@@ -101,15 +105,22 @@ class StatsTruth:
         if hasattr(self, "_state"):
             delattr(self, "_state")
             delattr(self, "_acc")
+            if self.weighted:
+                delattr(self, "_weights")
 
     def current_result(self):
         if not hasattr(self, "_state"):
             return torch.zeros(self._dim)
-        N = torch.bincount(self._acc, minlength=self._n_bins)
-        for d in self._reduce_dims:
-            N *= self._dim[d]
-        N = 1 / N
-        torch.nan_to_num_(N, nan=0.0, posinf=0.0, neginf=0.0)
+
+        if self.weighted:
+            mul_by = scatter(self._weights, self._acc, dim=0, dim_size=self._n_bins)
+            mul_by.sum(dim=self._reduce_dims)
+        else:
+            mul_by = torch.bincount(self._acc, minlength=self._n_bins)
+            for d in self._reduce_dims:
+                mul_by *= self._dim[d]
+        mul_by = 1 / mul_by
+        torch.nan_to_num_(mul_by, nan=0.0, posinf=0.0, neginf=0.0)
 
         if self._reduction == Reduction.RMS:
             state = self._state.square()
@@ -119,9 +130,11 @@ class StatsTruth:
         if len(self._reduce_dims) > 0:
             state = state.sum(dim=tuple(i + 1 for i in self._reduce_dims))
 
-        N = N.reshape((len(N),) + (1,) * (state.ndim - 1))
+        mul_by = mul_by.reshape((len(mul_by),) + (1,) * (state.ndim - 1))
 
-        out = N * scatter(state, self._acc, dim=0, reduce="sum", dim_size=self._n_bins)
+        out = mul_by * scatter(
+            state, self._acc, dim=0, reduce="sum", dim_size=self._n_bins
+        )
 
         if self._reduction == Reduction.RMS:
             out.sqrt_()
@@ -142,23 +155,48 @@ class StatsTruth:
 )
 @pytest.mark.parametrize("reduction", [Reduction.MEAN, Reduction.RMS])
 @pytest.mark.parametrize("do_accumulate_by", [True, False])
-def test_runstats(dim, reduce_dims, reduction, do_accumulate_by, allclose):
+@pytest.mark.parametrize("weighted", [(True, True), (True, False), (False, False)])
+def test_runstats(dim, reduce_dims, reduction, do_accumulate_by, weighted, allclose):
+    weighted, weight_extra_dims = weighted
+
+    if weighted and reduce_dims != tuple(range(len(reduce_dims))):
+        pytest.xfail("Cannot weight when reduce dims are not the first dims")
+
     n_batchs = (random.randint(1, 4), random.randint(1, 4))
-    truth_obj = StatsTruth(dim=dim, reduction=reduction, reduce_dims=reduce_dims)
-    runstats = RunningStats(dim=dim, reduction=reduction, reduce_dims=reduce_dims)
+    truth_obj = StatsTruth(
+        dim=dim, reduction=reduction, reduce_dims=reduce_dims, weighted=weighted
+    )
+    runstats = RunningStats(
+        dim=dim, reduction=reduction, reduce_dims=reduce_dims, weighted=weighted
+    )
 
     for n_batch in n_batchs:
         for _ in range(n_batch):
             batch = torch.randn((random.randint(1, 10),) + runstats.dim)
+
+            if weighted:
+                if weight_extra_dims:
+                    weight = torch.randn(
+                        (len(batch),) + tuple(runstats.dim[i] for i in reduce_dims)
+                    )
+                else:
+                    weight = torch.randn((len(batch),))
+            else:
+                weight = None
+
             if do_accumulate_by and random.choice((True, False)):
                 accumulate_by = torch.randint(
                     0, random.randint(1, 5), size=(batch.shape[0],)
                 )
-                truth = truth_obj.accumulate_batch(batch, accumulate_by=accumulate_by)
-                res = runstats.accumulate_batch(batch, accumulate_by=accumulate_by)
+                truth = truth_obj.accumulate_batch(
+                    batch, accumulate_by=accumulate_by, weight=weight
+                )
+                res = runstats.accumulate_batch(
+                    batch, accumulate_by=accumulate_by, weight=weight
+                )
             else:
-                truth = truth_obj.accumulate_batch(batch)
-                res = runstats.accumulate_batch(batch)
+                truth = truth_obj.accumulate_batch(batch, weight=weight)
+                res = runstats.accumulate_batch(batch, weight=weight)
             assert allclose(truth, res)
         assert allclose(truth_obj.current_result(), runstats.current_result())
         truth_obj.reset()
