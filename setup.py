8151a05951257171a9ea4094bce2941f79d5217f
Alby M <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-05-28 13:17:04 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="torch_runstats",
-    version="0.0.1",
+    version="0.1.0",
     url="https://github.com/mir-group/pytorch_runstats",
     description="Running/online statistics for PyTorch ",
     long_description=long_description,
