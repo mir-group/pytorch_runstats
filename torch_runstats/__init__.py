ebb92d80a8b2e3f7a4143c1ff2da0a15c57f6b47
Linux-cpp-lisp <1473644+Linux-cpp-lisp@users.noreply.github.com>
2021-11-16 15:49:23 -0500

    satisfy linter

--- torch_runstats/__init__.py
+++ torch_runstats/__init__.py
@@ -2,4 +2,4 @@ from ._version import __version__
 from ._runstats import RunningStats, Reduction
 from . import scatter
 
-__all__ = ["Reduction", "RunningStats", "scatter"]
+__all__ = ["__version__", "Reduction", "RunningStats", "scatter"]
